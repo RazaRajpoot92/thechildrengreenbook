932dd8844d3620876fda0103f35a841e4f1a4720
Raza <razarajpoot99598@gmail.com>
2023-12-27 19:32:34 +0500

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import imgData from '../data/image data/imageData'
+import { Link } from 'react-router-dom'
+
+
+const img3 = "https://thechildrengreenbook.net/assets/images/banners/banner-02.jpeg"
+
 
 const Hero = () => {
 
@@ -26,10 +31,18 @@ useEffect(()=>{
 
     }
   return (
-    <div style={bgstyle} className='w-[100vw] h-[70vh] md:w-[90vw] lg:w-[70vw]  rounded-lg m-auto mt-6 '>
-      heroh1
+    <div style={bgstyle} className='w-[100vw] gap-5 p-5 justify-center items-center flex h-[70vh] md:w-[90vw] lg:w-[70vw]  rounded-lg m-auto mt-6 '>
 
+        <div className='w-3/6 flex flex-col gap-7'>
+            <h1 className='text-6xl font-bold text-white'>Introduction</h1>
+            <p className='text-white font-semibold'>Although Climate Change is a global problem, yet Pakistan is one of the most affected countries, and therefore warrants some extraordinary steps to stem the tide. However, there could be no short-term solutions. In fact, considering the quantum of challenge the country is faced with, the entire nation needs to be sensitized.</p>
 
+            <Link className='border-[1px] py-2 border-green-500 text-white hover:bg-transparent hover:text-green-500  px-3 rounded-md bg-green-500' >Orger a copy</Link>
+        </div>
+
+        <div className='w-3/12 border-[1px] h-60 rounded-lg'>
+            <img src={img3} className='h-full rounded-lg ' alt="" />
+        </div>
     </div>
   )
 }
--- src/data/image data/imageData.js
+++ src/data/image data/imageData.js
@@ -1,8 +1,6 @@
-import img1 from '../../assets/education.jpg'
 import img2 from '../../assets/education2.jpg'
 
-//const img3 = "https://thechildrengreenbook.net/assets/images/banners/banner-02.jpeg"
-
+const img1 = 'https://images.pexels.com/photos/632280/pexels-photo-632280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 const imgData = [img1,img2,]
 
 export default imgData;
